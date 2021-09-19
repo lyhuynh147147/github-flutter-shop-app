d41545556d1e05feb2dbab830809ecb684877193
lyhuynh147147 <lyhuynh147147@email.com>
2021-09-19 17:05:40 +0700

    update pubspec

--- android/app/src/main/kotlin/com/example/phone_verification/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/phone_verification/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.example.phone_verification
 
 import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.engine.FlutterEngine
+//import io.flutter.plugins.firebase.core.FirebaseCorePlugin
+
+
+class MainActivity : FlutterActivity() { // You can keep this empty class or remove it. Plugins on the new embedding
+    // now automatically registers plugins.
+}
+/*
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+*/
